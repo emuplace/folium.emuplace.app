fdda7e42ea63d3b60f6a1099a6e3aba35267613f
Jarrod Norwell <official.antique@gmail.com>
2024-08-28 17:16:08 +0800

    Removed old home

--- src/Home/home_main.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Home from "./Home.tsx";
-
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-    <React.StrictMode>
-        <Home />
-    </React.StrictMode>
-);
