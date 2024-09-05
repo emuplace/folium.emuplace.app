86186376bbdd07fc42388024ea6cdbf69a0188ab
Jarrod Norwell <official.antique@gmail.com>
2024-09-06 00:06:04 +0800

    Updated for release

--- src/NewHome/NewHome.tsx
+++ src/NewHome/NewHome.tsx
@@ -1,6 +1,6 @@
 import '@mantine/core/styles.css';
 import {
-    Anchor, Button, Flex,
+    Anchor, Badge, Center, Flex,
     MantineProvider,
     Stack, Text, Title
 } from '@mantine/core';
@@ -20,13 +20,19 @@ export default function NewHome() {
                     <Text c={'dimmed'} ta={'center'}>
                         Beautifully designed, high performing multi-system emulation in the palm of your hands
                     </Text>
+                    <Center>
+                        <Badge color={'gold'}>
+                            Friday, 7th September
+                        </Badge>
+                    </Center>
+                    {/*
                     <Flex align={'center'} gap={'md'} justify={'center'} direction={'row'} wrap={'wrap'}>
                         <Button component='a' href='https://apps.apple.com/au/app/folium/id6498623389' color='blue' radius={'xl'} variant='filled'>Open App Store</Button>
                         <Button component='a' href='https://discord.gg/jv3wmCUTw7' color='violet' radius={'xl'}>Join Discord</Button>
                         <Button component='a' href='https://github.com/folium-app' color='gray' radius={'xl'} variant='filled'>Open GitHub</Button>
                         <Button component='a' href='/rewrite' radius={'xl'} variant='filled'>Browse Rewrite</Button>
                         <Button component='a' href='/ipas/Folium-v1.6-Sideload.ipa.zip' color='cyan' radius={'xl'} variant='filled'>Sideload</Button>
-                    </Flex>
+                    </Flex>*/}
                 </Stack>
             </Flex>
         </MantineProvider>
