bfeb3caf2d0506da737da942a7ffc03f43713a15
Jarrod Norwell <official.antique@gmail.com>
2024-09-08 01:56:56 +0800

    Added latest release

--- src/NewHome/NewHome.tsx
+++ src/NewHome/NewHome.tsx
@@ -1,6 +1,6 @@
 import '@mantine/core/styles.css';
 import {
-    Anchor, Badge, Button, Center, Flex,
+    Anchor, Button, Flex,
     MantineProvider,
     Stack, Text, Title
 } from '@mantine/core';
@@ -20,24 +20,13 @@ export default function NewHome() {
                     <Text c={'dimmed'} ta={'center'}>
                         Beautifully designed, high performing multi-system emulation in the palm of your hands
                     </Text>
-                    <Center>
-                        <Button color='grape' component='a' href='https://jarrodnorwell.gitbook.io/folium/getting-started' radius={'xl'}>
-                            Getting Started
-                        </Button>
-                        </Center>
-                    {/*
                     <Flex align={'center'} gap={'md'} justify={'center'} direction={'row'} wrap={'wrap'}>
                         <Button component='a' href='https://apps.apple.com/au/app/folium/id6498623389' color='blue' radius={'xl'} variant='filled'>Open App Store</Button>
                         <Button component='a' href='https://discord.gg/jv3wmCUTw7' color='violet' radius={'xl'}>Join Discord</Button>
-                        <Button component='a' href='https://github.com/folium-app' color='dark' radius={'xl'} variant='filled'>Getting Started</Button>
+                        <Button component='a' href='https://jarrodnorwell.gitbook.io/folium/getting-started' color='grape' radius={'xl'} variant='filled'>Getting Started</Button>
                         <Button component='a' href='https://github.com/folium-app' color='dark' radius={'xl'} variant='filled'>Open GitHub</Button>
                         <Button component='a' href='ipas/folium-v1.7-sideload.ipa.zip' color='teal' radius={'xl'} variant='filled'>Sideload</Button>
-                    </Flex>*/}
-                    <Center>
-                        <Badge>
-                            Waiting for Review
-                        </Badge>
-                    </Center>
+                    </Flex>
                 </Stack>
             </Flex>
         </MantineProvider>
