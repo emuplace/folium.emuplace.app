96bfee06f0a980185a78de21b49457e20207ed33
Jarrod Norwell <official.antique@gmail.com>
2024-11-04 20:25:57 +0800

    Updated v1.13 changes

--- src/NewHome/NewHome.tsx
+++ src/NewHome/NewHome.tsx
@@ -2,7 +2,8 @@ import '@mantine/core/styles.css';
 import {
     Anchor, Button, Flex, Group, Badge,
     MantineProvider, List, Accordion,
-    Stack, Text, Title
+    Stack, Text, Title,
+    Container
 } from '@mantine/core';
 import classes from './NewHome.module.css';
 import { theme } from '../theme';
@@ -19,13 +20,53 @@ export default function NewHome() {
                     tertiaryText: null
                 },
                 {
+                    secondaryText: 'Added the ability to open the Cheats screen when in-game for the Cytrus core',
+                    tertiaryText: null
+                },
+                {
+                    secondaryText: 'Added a new log level setting for the Cytrus core to help with debugging',
+                    tertiaryText: null
+                },
+                {
+                    secondaryText: 'Added basic support for DSi mode for the Grape core by updating NooDS',
+                    tertiaryText: null
+                },
+                {
+                    secondaryText: 'Added landscape support for the Grape and Mango core on all iPhone models (iPad is WIP)',
+                    tertiaryText: null
+                },
+                {
                     secondaryText: 'Changed the number of columns for both Authentication and the Library screens',
+                    tertiaryText: null
+                },
+                {
+                    secondaryText: 'Changed how the keyboard works for the Cytrus core enabling support for both iPad and iPhone',
                     tertiaryText: null
                 },
                 {
                     secondaryText: 'Fixed an issue where all iPad models could not receive touch',
                     tertiaryText: null
-                }
+                },
+                {
+                    secondaryText: 'Fixed an issue where the on-screen controller would display again when the device orientation changes while a Bluetooth controller is connected',
+                    tertiaryText: null
+                },
+                {
+                    secondaryText: 'Fixed a crash when installing system applications for the Cytrus core caused by applications with invalid artwork',
+                    tertiaryText: null
+                },
+                {
+                    secondaryText: 'Fixed an issue where thumbsticks in custom skins would not use their assigned background artwork',
+                    tertiaryText: null
+                },
+                {
+                    secondaryText: 'Fixed an issue where Nintendo DS game titles would be duplicated in the Library screen',
+                    tertiaryText: null
+                },
+                {
+                    secondaryText: 'Updated the MoltenVK dependency which may fix some graphical issues',
+                    tertiaryText: null
+                },
             ]
         },
         {
@@ -113,7 +154,7 @@ export default function NewHome() {
 
     return (
         <MantineProvider theme={theme} forceColorScheme={date.getHours() >= 7 && date.getHours() <= 19 ? "light" : "dark"}>
-            <Flex align={'center'} justify={'center'} h={'100vh'} px={'md'} w={'100vw'}>
+            <Container my={'xl'}>
                 <Stack>
                     <Anchor href='https://twitter.com/antique_codes' ta={'center'} target='_blank'>
                         @antique_codes
@@ -129,7 +170,7 @@ export default function NewHome() {
                         <Button component='a' href='https://discord.gg/jv3wmCUTw7' color='violet' radius={'xl'}>Join Discord</Button>
                         {/*<Button component='a' href='https://jarrodnorwell.gitbook.io/folium/getting-started' color='grape' radius={'xl'} variant='filled'>Getting Started</Button>*/}
                         <Button component='a' href='https://github.com/folium-app' color='gray' radius={'xl'} variant='filled'>Open GitHub</Button>
-                        <Button component='a' href='https://github.com/emuplace/folium.emuplace.app/releases/download/v1.13/folium-sideload-v1.13.ipa.zip' color='teal' radius={'xl'} variant='filled'>Sideload</Button>
+                        <Button component='a' href='https://github.com/emuplace/folium.emuplace.app/releases/download/v1.13/folium-sideload-v1.13.ipa.zip' color='teal' radius={'xl'} variant='filled'>Sideload (v1.13)</Button>
                     </Flex>
                     <Title order={2}>
                         Changes
@@ -138,7 +179,7 @@ export default function NewHome() {
                         {items}
                     </Accordion>
                 </Stack>
-            </Flex>
+            </Container>
         </MantineProvider>
     );
 }
