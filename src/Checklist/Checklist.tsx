998b249e5102602bc52d2f20daef076df7897a4e
Jarrod Norwell <official.antique@gmail.com>
2024-12-05 22:00:24 +0800

    Added colours, more features

--- src/Checklist/Checklist.tsx
+++ src/Checklist/Checklist.tsx
@@ -41,15 +41,22 @@ export default function Checklist() {
                 </Text>
                 <Space h={'sm'} />
                 <SimpleGrid>
-                    <Tooltip label='YUV422 is not fully supported yet'>
-                        <Checkbox label={'Camera'} indeterminate onClick={(event) => event.preventDefault()} />
+                    <Checkbox label={'Bootable OS'} checked onClick={(event) => event.preventDefault()} />
+                    <Tooltip label='YUV422 pixel format is not fully supported yet'>
+                        <Checkbox color='orange' label={'Camera'} checked onClick={(event) => event.preventDefault()} variant='outline' />
+                    </Tooltip>
+                    <Tooltip label='Added, editing and removing cheats in-app is not supported yet'>
+                        <Checkbox color='orange' label={'Cheats'} checked onClick={(event) => event.preventDefault()} variant='outline' />
                     </Tooltip>
                     <Tooltip label='Rotation data is currently sent to the 3DS backwards'>
-                        <Checkbox label={'Gyroscope'} indeterminate onClick={(event) => event.preventDefault()} />
+                        <Checkbox color='orange' label={'Gyroscope'} checked onClick={(event) => event.preventDefault()} variant='outline' />
                     </Tooltip>
                     <Checkbox label={'Microphone'} checked onClick={(event) => event.preventDefault()} />
+                    <Tooltip label='Support for chat rooms and direct connections is not done yet'>
+                        <Checkbox color='orange' label={'Multiplayer'} checked onClick={(event) => event.preventDefault()} variant='outline' />
+                    </Tooltip>
                     <Tooltip label='Step data is not read correctly by the 3DS yet'>
-                        <Checkbox label={'Pedometer'} indeterminate onClick={(event) => event.preventDefault()} />
+                        <Checkbox color='red' label={'Pedometer'} indeterminate onClick={(event) => event.preventDefault()} variant='outline' />
                     </Tooltip>
                     <Checkbox label={'Speakers'} checked onClick={(event) => event.preventDefault()} />
                 </SimpleGrid>
@@ -65,8 +72,9 @@ export default function Checklist() {
                 </Text>
                 <Space h={'sm'} />
                 <SimpleGrid>
+                    <Checkbox label={'Bootable OS'} checked onClick={(event) => event.preventDefault()} />
                     <Tooltip label='Microphone data does not get read by the DS yet'>
-                        <Checkbox label={'Microphone'} onClick={(event) => event.preventDefault()} />
+                        <Checkbox color='red' label={'Microphone'} indeterminate onClick={(event) => event.preventDefault()} variant='outline' />
                     </Tooltip>
                     <Checkbox label={'Speakers'} checked onClick={(event) => event.preventDefault()} />
                 </SimpleGrid>
@@ -82,6 +90,7 @@ export default function Checklist() {
                 </Text>
                 <Space h={'sm'} />
                 <SimpleGrid>
+                    <Checkbox label={'Bootable OS'} checked onClick={(event) => event.preventDefault()} />
                     <Checkbox label={'Speakers'} checked onClick={(event) => event.preventDefault()} />
                 </SimpleGrid>
 
@@ -94,6 +103,10 @@ export default function Checklist() {
                 <Text c={'dimmed'}>
                     Super Nintendo Entertainment System
                 </Text>
+                <Space h={'sm'} />
+                <SimpleGrid>
+                    <Checkbox label={'Speakers'} checked onClick={(event) => event.preventDefault()} />
+                </SimpleGrid>
             </Container>
         </MantineProvider>
     );
